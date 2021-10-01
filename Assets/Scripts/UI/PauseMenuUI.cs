787266c97f763800e6f851f342a77be9653a0172
Jorge <gd04jorge@thecea.ca>
2021-09-30 23:38:46 -0700

    Fixed Gameplay Scene UI

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class PauseMenuUI : MonoBehaviour
@@ -9,10 +10,32 @@ public class PauseMenuUI : MonoBehaviour
     [SerializeField]
     private GameObject _instructions;
 
+    public GameObject continueButton;
+    public GameObject instructionsButton;
+    public GameObject exitButton;
+    public GameObject exitTopButton;
+    public EventSystem _eventSystem;
+
+    private bool _isPaused;
+
     private void OnEnable()
     {
         Time.timeScale = 0f;
+        _isPaused = true;
     }
+
+    /**private void OnDisable()
+    {
+        _isPaused = false;
+    }**/
+
+    /**private void Update()
+    {
+        if(_isPaused == true)
+        {
+            exitTopButton.SetActive(false);
+        }
+    }**/
 
     public void OnContinuePressed()
     {
@@ -23,6 +46,21 @@ public class PauseMenuUI : MonoBehaviour
     public void OnInstructionsPressed()
     {
         _instructions.SetActive(true);
+        continueButton.SetActive(false);
+        instructionsButton.SetActive(false);
+        exitButton.SetActive(false);
+        exitTopButton.SetActive(true);
+        _eventSystem.SetSelectedGameObject(exitTopButton);
+    }
+
+    public void ExitTopPressed()
+    {
+        _instructions.SetActive(false);
+        continueButton.SetActive(true);
+        instructionsButton.SetActive(true);
+        exitButton.SetActive(true);
+        exitTopButton.SetActive(false);
+        _eventSystem.SetSelectedGameObject(instructionsButton);
     }
 
     public void OnQuitPressed()
